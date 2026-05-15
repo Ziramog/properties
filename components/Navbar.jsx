d3aa33930687d1c0d1e3650389fabf8761b6c1dd
Ziramog <fullpowerok@gmail.com>
2026-05-15 20:20:50 -0300

    bump desktop logo before scroll +10% (252x84→277x92)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -89,9 +89,9 @@ const Navbar = () => {
               className="brightness-0 invert transition-all duration-300 group-hover:opacity-70"
               src={isHeroPage && !isScrolled ? '/images/LOGO R&R 2023.png' : '/images/ISOTIPO R&R-Photoroom.png'}
               alt="Roggero & Roma"
-              width={isHeroPage && !isScrolled ? 252 : 120}
-              height={isHeroPage && !isScrolled ? 84 : 40}
-              style={{ height: isHeroPage && !isScrolled ? '84px' : '40px', width: 'auto' }}
+              width={isHeroPage && !isScrolled ? 277 : 120}
+              height={isHeroPage && !isScrolled ? 92 : 40}
+              style={{ height: isHeroPage && !isScrolled ? '92px' : '40px', width: 'auto' }}
             />
           </Link>
 
