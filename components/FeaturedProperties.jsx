1490340ab500020503ed19edadf70098c596ecd4
Ziramog <fullpowerok@gmail.com>
2026-05-28 13:51:16 -0300

    Reduce homepage section spacing to 15px

--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -19,7 +19,7 @@ const FeaturedProperties = ({ properties = [] }) => {
   if (properties.length === 0) return null;
 
   return (
-    <section className="pt-[23px] pb-[23px]" ref={ref}>
+    <section className="pt-[15px] pb-[15px]" ref={ref}>
       <SectionBox className="px-3 md:px-8 py-16 md:py-24">
         {/* Section Header */}
         <div className="text-center mb-10 md:mb-14 js-animate">
