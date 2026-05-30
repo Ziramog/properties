8f5211e609b5ebd6e1005cecb9b295e2469c300d
Ziramog <fullpowerok@gmail.com>
2026-05-30 18:19:24 -0300

    Map pin: use #db7340 orange for outer shape, #c06030 for inner circle

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -242,8 +242,8 @@ const PropertyMap = ({ property }) => {
       >
         <Marker longitude={lng} latitude={lat} anchor='bottom'>
           <svg width="44" height="44" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-            <path d="m32 0a24.028 24.028 0 0 0 -24 24c0 16.228 22.342 38.756 23.293 39.707a1 1 0 0 0 1.414 0c.951-.951 23.293-23.479 23.293-39.707a24.028 24.028 0 0 0 -24-24z" fill="#E94560"/>
-            <circle cx="32" cy="24" fill="#cc3a52" r="13"/>
+            <path d="m32 0a24.028 24.028 0 0 0 -24 24c0 16.228 22.342 38.756 23.293 39.707a1 1 0 0 0 1.414 0c.951-.951 23.293-23.479 23.293-39.707a24.028 24.028 0 0 0 -24-24z" fill="#db7340"/>
+            <circle cx="32" cy="24" fill="#c06030" r="13"/>
             <circle cx="32" cy="24" fill="#fff" opacity="0.25" r="6"/>
           </svg>
         </Marker>
